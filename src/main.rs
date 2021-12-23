38648e173d198e925ab9da8be110482a0a3c429f
Arash Sal Moslehian <arashsm79@yahoo.com>
2021-12-23 09:49:39 +0330

    Read input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,71 @@
-fn main {
-    println!("Hi");
+use std::error::Error;
+
+struct CSP {
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let test_case_path = std::env::args()
+        .nth(1)
+        .expect("Please provide a test case path as command line argument.");
+
+    let csp = init_problem(test_case_path);
+
+
+
+    Ok(())
+}
+
+fn init_problem(test_case_path: String) -> Result<CSP,  Box<dyn Error>> {
+    let test_case_lines: Vec<String> = std::fs::read_to_string(test_case_path)?
+        .lines()
+        .map(|l| l.parse::<String>().unwrap())
+        .collect();
+
+    let board_size: Vec<usize> = test_case_lines
+        .get(0)
+        .expect("Wrong input format. First line must be the size of the board")
+        .split(" ")
+        .map(|tok| tok.parse::<usize>().unwrap())
+        .collect();
+    let row_size = board_size[0];
+    let col_size = board_size[1];
+
+    let row_pos_poles: Vec<u32> = test_case_lines
+        .get(1)
+        .expect("Wrong input format. Second line must be the number of positive poles per row")
+        .split(" ")
+        .map(|tok| tok.parse::<u32>().unwrap())
+        .collect();
+
+    let row_neg_poles: Vec<u32> = test_case_lines
+        .get(2)
+        .expect("Wrong input format. Third line must be the number of negative poles per row")
+        .split(" ")
+        .map(|tok| tok.parse::<u32>().unwrap())
+        .collect();
+
+    let col_pos_poles: Vec<u32> = test_case_lines
+        .get(3)
+        .expect("Wrong input format. Forth line must be the number of positive poles per column")
+        .split(" ")
+        .map(|tok| tok.parse::<u32>().unwrap())
+        .collect();
+
+    let col_neg_poles: Vec<u32> = test_case_lines
+        .get(4)
+        .expect("Wrong input format. Fifth line must be the number of negative poles per column")
+        .split(" ")
+        .map(|tok| tok.parse::<u32>().unwrap())
+        .collect();
+
+    let raw_board: Vec<Vec<u8>> = test_case_lines
+        .get(5..(5 + row_size) as usize)
+        .expect("Wrong input format. Not enough rows specified")
+        .iter()
+        .map(|line| {
+            line.split(" ")
+                .map(|tok| tok.parse::<u8>().unwrap())
+                .collect::<Vec<u8>>()
+        })
+        .collect::<Vec<Vec<u8>>>();
 }
